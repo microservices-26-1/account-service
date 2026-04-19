35195a6afffffeb43f472d6d6b8563838e5ef69c
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2026-04-19 20:07:45 -0300

    account-service implemented

---
+++ src/main/java/product_store/account/Account.java
@@ -0,0 +1,17 @@
+package product_store.account;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Builder @Accessors(chain = true, fluent = true)
+public class Account {
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String passwordSha256;
+    
+}
---
+++ src/main/java/product_store/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package product_store.account;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<AccountModel, String>  {
+
+    public Optional<AccountModel> findByEmailAndPasswordSha256(String email, String sha256);
+    
+}
